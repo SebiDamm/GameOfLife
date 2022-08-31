5c16ca7565c138c9eb681050821e5913a87dbbea
sebidamm <2005sebastrian.sd@gmail.com>
2022-08-31 17:53:14 +0200

    Game of Life

--- src/main/java/com/example/gameoflife/HelloApplication.java
+++ src/main/java/com/example/gameoflife/HelloApplication.java
@@ -2,23 +2,62 @@ package com.example.gameoflife;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Stack;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+        Button xCoordinateButton = new Button();
+        xCoordinateButton.setOnMouseClicked(event -> {
+            System.out.println(event.getX());
+        });
+        xCoordinateButton.setText("X Coordinates");
+        xCoordinateButton.setAlignment(Pos.CENTER_RIGHT);
+
+        Label label = new Label("Activity: ");
+
+        HBox hBox = new HBox();
+        hBox.getChildren().addAll(label,xCoordinateButton);
+        hBox.setPadding(new Insets(40,40,40,40));
+        hBox.setSpacing(40);
+
+
+        BorderPane bPane = new BorderPane();
+        bPane.setTop(new TextField("Top"));
+        bPane.setBottom(new TextField("Bottom"));
+        bPane.setLeft(new TextField("Left"));
+        bPane.setRight(new TextField("Right"));
+        bPane.setCenter(hBox);
+        bPane.setBackground(new Background(new BackgroundFill(Color.rgb(255,255,0),CornerRadii.EMPTY,Insets.EMPTY)));
+//        BorderPane.setAlignment(hBox,Pos.CENTER);
+//
+//        AnchorPane.setTopAnchor(bPane,0.);
+//        AnchorPane.setLeftAnchor(bPane,0.);
+//        AnchorPane.setRightAnchor(bPane,0.);
+//        AnchorPane.setBottomAnchor(bPane,0.);
+
+        Scene scene = new Scene(bPane, 320, 240);
+
+
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-        //launch();
+        launch();
 
     }
 }
--- src/main/java/com/example/gameoflife/Main.java
+++ src/main/java/com/example/gameoflife/Main.java
@@ -2,14 +2,24 @@ package com.example.gameoflife;
 
 public class Main {
     public static void main(String[] args) {
-        Spielbrett spielbrett = new Spielbrett(10,10);
+        Spielbrett spielbrett = new Spielbrett(50,20);
 
-        spielbrett.createLife(2,6);
-        spielbrett.createLife(2,5);
-        spielbrett.createLife(2,7);
+//        spielbrett.createLife(2,2);
+//        spielbrett.createLife(2,1);
+//        spielbrett.createLife(3,3);
+//        spielbrett.createLife(3,4);
+//        spielbrett.createLife(1,3);
+//        spielbrett.createLife(4,2);
 
-        spielbrett.printBoard();
-        spielbrett.nextGeneration();
-        spielbrett.printBoard();
+        spielbrett.createLife(1,3);
+        spielbrett.createLife(2,3);
+        spielbrett.createLife(2,1);
+        spielbrett.createLife(3,2);
+        spielbrett.createLife(3,3);
+
+        for (int i = 0; i < 100; i++) {
+            spielbrett.printBoard();
+            spielbrett.nextGeneration();
+        }
     }
 }
